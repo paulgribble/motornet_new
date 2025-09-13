f50b0b1c2636a0c832d7eeb520e0da655465f2fa
Paul Gribble <pgribble@uwo.ca>
2025-09-12 21:07:42 -0400

    misc

--- demo.py
+++ demo.py
@@ -55,7 +55,7 @@ policy, optimizer = create_policy(env, inputs, device,
                                   optimizer_mod = optimizer_mod, 
                                   learning_rate = learning_rate)
 
-loss_function = my_loss.calculate_loss_kashefi_2025
+loss_function = my_loss.calculate_loss_michaels_2025_nature
 
 task.run_mode = 'train' # random reaches
 
